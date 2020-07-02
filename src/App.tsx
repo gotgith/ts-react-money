a01502b49b309bd285a852b407c2e87bbd91a7f1
JH <18468107890@163.com>
2020-07-02 13:56:26 +0800

    add common style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
+import './index.scss';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+`;
 
 
 function App() {
   return (
-    <div>
-      你好
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="money"></Redirect>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <h2>无此页面</h2>;
+}
+
+function Statistics() {
+  return <h2>标签页面</h2>;
+}
+
+function Tags() {
+  return <h2>记账页面</h2>;
+}
+
+function Money() {
+  return <h2>统计页面</h2>;
 }
 
 export default App;
