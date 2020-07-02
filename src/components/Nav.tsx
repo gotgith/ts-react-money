3cef57a306d8e837cf04bd6ada6bbf895c0d31b8
JH <18468107890@163.com>
2020-07-02 14:48:48 +0800

    add icons and styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/chart.svg');
+require('icons/money.svg');
+require('icons/taps.svg');
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -9,7 +13,17 @@ const NavWrapper = styled.nav`
     width: 100%;
     display:flex;
     justify-content: space-around;
-    padding: 16px;
+    li{
+      padding: 4px;
+      display:flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
+    }
   }
 `;
 
@@ -18,12 +32,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#taps"></use>
+          </svg>
           <Link to="/tags">标签页面</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"></use>
+          </svg>
           <Link to="/money">记账页面</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart"></use>
+          </svg>
           <Link to="/statistics">统计页面</Link>
         </li>
       </ul>
