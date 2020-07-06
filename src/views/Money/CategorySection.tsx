4143f5765e3ea906695d3db9592eedabb57e2822
JH <18468107890@163.com>
2020-07-06 09:58:45 +0800

    numberPad 功能 wip

---
+++ src/views/Money/CategorySection.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import React, {useState} from 'react';
+
+const Wrapper = styled.section`
+  font-size: 24px;
+  > ul{
+    display:flex;
+    background:#c4c4c4;
+    > li {
+      width: 50%; 
+      text-align:center;
+      padding: 16px 0;
+      position:relative;
+      &.selected::after{
+        content: '';
+        display:block; 
+        height: 3px;
+        background:#333;
+        position:absolute;
+        bottom:0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
+`;
+
+const CategorySection: React.FC = () => {
+    const categoryMap = {'-': '支出', '+': '收入'};
+    type Keys = keyof typeof categoryMap
+    const [categoryList] = useState<Keys[]>(['-', '+']);
+    const [category, setCategory] = useState<string>('-');
+    return (
+      <Wrapper>
+        <ul>
+          {categoryList.map(item =>
+            <li className={category === item ? 'selected' : ''}
+                onClick={() => {setCategory(item);}} key={item}>
+              {categoryMap[item]}
+            </li>
+          )}
+        </ul>
+      </Wrapper>
+    );
+  }
+;
+
+export {CategorySection};
---
+++ src/views/Money/NotesSection.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import React, {useRef, useState} from 'react';
+
+const Wrapper = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
+
+const NotesSection: React.FunctionComponent = () => {
+  const [note, setNote] = useState<string>('');
+  const refInput = useRef<HTMLInputElement>(null);
+  const onBlur = () => {
+    if (refInput.current) {
+      setNote(refInput.current.value);
+      console.log(refInput.current.value);
+    }
+  };
+  return (
+    <Wrapper>
+      <label>
+        <span>备注</span>
+        <input type="text" placeholder="在这里添加备注" ref={refInput} defaultValue={note} onBlur={onBlur}/>
+      </label>
+    </Wrapper>
+  );
+};
+
+export {NotesSection};
+
