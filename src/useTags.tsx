d13e72156c31367c8f0d5120a32abd9e7926a6f5
JH <18468107890@163.com>
2020-07-09 16:14:41 +0800

    更改 tag 名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,7 +11,25 @@ const defaultTags = [
 const useTags = () => { //封装一个自定义的 hook(在函数里面使用useState等一系列，返回读写接口到外面去，这个函数就是一个自定义hook)
   const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
-  return {tags, setTags, findTag};
+  const findTagIndex = (id: number) => {
+    let result = -1;  //若是直接返回i，会存在找不到的情况，这样返回的就是tags的length长度，因此需要标记一下，找不到返回result的默认值
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+      return result;
+    }
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    let index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    // 把 tagsClone 的第 index 删除，换成{id: id, name: obj.name}
+    tagsClone.splice(index, 1, {id: id, name: obj.name});  //splice的返回是删除的元素，因此不需要接收返回值，只需要改变tags
+    setTags(tagsClone);
+  };
+  return {tags, setTags, findTag, updateTag, findTagIndex};
 };
 
 export {useTags};
